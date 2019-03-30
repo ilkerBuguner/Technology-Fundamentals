49919a06c36075bb7342472622823b094f340844
İlker Bugüner <ilker.buguner@gmail.com>
2019-03-30 17:41:34 +0200

    Added basic web homework

---
+++ 09-Basic-Web/Lab/Phonebook/Data/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Data.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
